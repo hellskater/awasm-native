91e78d60f05a5216edc6ff1cf90db29d7a8687f6
hellskater <ksrinivasrao531@gmail.com>
2022-10-20 19:13:50 +0530

    api error fix

--- src/axiosClient.ts
+++ src/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Axios common config
 
 const axiosClient = axios.create({
-  baseURL: '/'
+  baseURL: 'https://awasm-native.vercel.app/'
 });
 
 export default axiosClient;
