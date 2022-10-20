9c6e4dfe2c42d64df12048c94ab59026307d9288
hellskater <ksrinivasrao531@gmail.com>
2022-10-20 19:58:00 +0530

    cert url added in env

--- src/pages/api/db.ts
+++ src/pages/api/db.ts
@@ -13,8 +13,7 @@ const serviceAccount = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-jigbo%40awasm-native.iam.gserviceaccount.com'
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL
 };
 
 // If firebase-admin app is not initialized then initialize it
